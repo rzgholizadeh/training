991ee1d6d68d6c2994c6a2eaf5e13c60c5be5afd
Reza <rzgholizadeh@gmail.com>
2020-02-15 17:37:13 -0700

    exercise update
    Updated exercise description to add form validation practice and update the solution too

--- Java-spring-hibernate-course/spring-mvc/e2/spring-mvc-exercise--2/src/learning/spring/mvc/CustomerController.java
+++ Java-spring-hibernate-course/spring-mvc/e2/spring-mvc-exercise--2/src/learning/spring/mvc/CustomerController.java
@@ -2,15 +2,28 @@ package learning.spring.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+	// add an initbinder to convert trim input strings
+	// remove leading and trailing whitespace
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -20,6 +33,9 @@ public class CustomerController {
 
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+
+		System.out.println("last name: |" + theCustomer.getLastName() + "|");
+
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
