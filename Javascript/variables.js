2d56b90ccf4ea3fe647618b4c8d6f2fa538bae68
Reza <rzgholizadeh@gmail.com>
2020-03-16 20:44:56 -0600

    JS: variables and constants

---
+++ Javascript/variables.js
@@ -0,0 +1,25 @@
+// naming convention: Camel Case
+
+// let: specific to scope. let is newer than var!
+let age = 10;
+age += 1;
+age -= 1;
+age *= 5;
+age /= 5;
+console.log("age = " + age);
+
+let avgRating = 2.1;
+console.log("Average Rating = " + avgRating);
+
+let counter = 0;
+counter++;
+counter--;
+console.log("Counter = " + counter);
+
+// const: it is a constant
+const chairs = 2;
+console.log("Chairs = " + chairs);
+
+// var: it is outdated! it is generic scope
+var height = 100;
+console.log("Height = " + height);
