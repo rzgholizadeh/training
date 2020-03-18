e401edadc160bff4e3bc259e1d2c5cf2728c43e3
Reza <rzgholizadeh@gmail.com>
2020-03-17 19:46:28 -0600

    JS: Math object

---
+++ Javascript/math.js
@@ -0,0 +1,10 @@
+// Math object
+console.log(Math.PI);
+console.log(Math.floor(2.123));
+console.log(Math.round(3.2));
+
+// Math.random() --> from zero to one not including 1
+console.log(Math.random());
+
+// a dice roll
+console.log(Math.floor(Math.random() * 6) + 1);
