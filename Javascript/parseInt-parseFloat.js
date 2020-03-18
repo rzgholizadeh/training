51b713f6ae790240fdf15aa8618df59bf71c68d4
Reza <rzgholizadeh@gmail.com>
2020-03-17 19:57:00 -0600

    JS: parseInt and parseFloat

---
+++ Javascript/parseInt-parseFloat.js
@@ -0,0 +1,8 @@
+// parseInt returns the integer equal to the string we pass into
+// parseFloat return all number (if any floating point)
+console.log(parseInt("2.2"));
+console.log(parseFloat("2.2"));
+console.log(parseFloat("2"));
+console.log(parseInt("    23"));
+console.log(parseInt("44someOther Character"));
+
