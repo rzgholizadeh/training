596b87a875ae1a336f5729218cbb3d883092cc7a
Reza <rzgholizadeh@gmail.com>
2020-03-19 16:44:08 -0600

    JS: foreach function for iterateables

---
+++ Javascript/functions/collection-functions.js
@@ -0,0 +1,5 @@
+// foreach: the first input is the element, the second is the index
+const someArr = [10, 11, 12, 13, 14];
+someArr.forEach(function(element, index) {
+	console.log(`index: ${index}, element: ${element}`);
+});
