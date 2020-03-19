8b1fc0b25f99473d62438eb49041c3d7c0839194
Reza <rzgholizadeh@gmail.com>
2020-03-19 16:58:39 -0600

    JS: map function basics

--- Javascript/functions/collection-functions.js
+++ Javascript/functions/collection-functions.js
@@ -1,5 +1,31 @@
 // foreach: the first input is the element, the second is the index
 const someArr = [10, 11, 12, 13, 14];
+const words = ["hi", "apple", "orange"];
 someArr.forEach(function(element, index) {
 	console.log(`index: ${index}, element: ${element}`);
 });
+
+// map: create a new array from an existing one
+const doubleArr = someArr.map(function(num) {
+	return num * 2;
+});
+console.log(someArr);
+console.log(doubleArr);
+
+const evenNum = someArr.map(function(n) {
+	return {
+		value: n,
+		isEven: n % 2 === 0
+	};
+});
+console.log(evenNum);
+
+const capWords = words.map(function(word) {
+	return word
+		.toUpperCase()
+		.split("")
+		.join(".");
+});
+console.log(words);
+console.log(capWords);
+
