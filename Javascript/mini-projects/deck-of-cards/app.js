9339d9cc283a6ee0b21ebe0cbdc2f964fa13d29c
Reza <rzgholizadeh@gmail.com>
2020-03-19 23:38:14 -0600

    JS: add a deck factory for the deck-of-cards app

--- Javascript/mini-projects/deck-of-cards/app.js
+++ Javascript/mini-projects/deck-of-cards/app.js
@@ -1,36 +1,46 @@
-const myDeck = {
-	deck: [],
-	drawnCards: [],
-	suits: ["hearts", "diamonds", "spades", "clubs"],
-	values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
-	initializeDeck() {
-		const { deck, suits, values } = this;
-		for (let value of values.split(",")) {
-			for (let suit of suits) {
-				deck.push({ value, suit });
+// a deck factory
+const makeDack = () => {
+	return {
+		deck: [],
+		drawnCards: [],
+		suits: ["hearts", "diamonds", "spades", "clubs"],
+		values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+		initializeDeck() {
+			const { deck, suits, values } = this;
+			for (let value of values.split(",")) {
+				for (let suit of suits) {
+					deck.push({ value, suit });
+				}
+			}
+		},
+		drawCard() {
+			const card = this.deck.pop();
+			this.drawnCards.push(card);
+			return card;
+		},
+		drawMultiple(numCards) {
+			const cards = [];
+			for (let i = 0; i < numCards; i++) {
+				cards.push(this.drawCard());
+			}
+			return cards;
+		},
+		shuffle() {
+			const { deck } = this;
+			// loop over array backwards
+			for (let i = deck.length - 1; i > 0; i--) {
+				// pick random index before current element
+				let j = Math.floor(Math.random() * (i + 1));
+				// swap
+				[deck[i], deck[j]] = [deck[j], deck[i]];
 			}
 		}
-	},
-	drawCard() {
-		const card = this.deck.pop();
-		this.drawnCards.push(card);
-		return card;
-	},
-	drawMultiple(numCards) {
-		const cards = [];
-		for (let i = 0; i < numCards; i++) {
-			cards.push(this.drawCard());
-		}
-		return cards;
-	},
-	shuffle() {
-		const { deck } = this;
-		// loop over array backwards
-		for (let i = deck.length - 1; i > 0; i--) {
-			// pick random index before current element
-			let j = Math.floor(Math.random() * (i + 1));
-			// swap
-			[deck[i], deck[j]] = [deck[j], deck[i]];
-		}
-	}
+	};
 };
+
+const myDeck = makeDack();
+myDeck.initializeDeck();
+myDeck.shuffle();
+const h1 = myDeck.drawMultiple(2);
+const h2 = myDeck.drawMultiple(2);
+const h3 = myDeck.drawMultiple(5);
