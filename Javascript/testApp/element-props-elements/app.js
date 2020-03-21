48b9f69eafb419f892c763591f533e1e9e60fcb6
Reza <rzgholizadeh@gmail.com>
2020-03-20 20:33:35 -0600

    JS: change style of elements using js

--- Javascript/testApp/element-props-elements/app.js
+++ Javascript/testApp/element-props-elements/app.js
@@ -9,3 +9,23 @@ for (let li of allLis) {
 	li.innerHTML = "another new <b> TEXT </b>";
 }
 
+// styles are camel cased
+const h1 = document.querySelector("h1");
+
+// add style value inline (html)
+// they override css style. it is better not to use them
+//h1.style.color = "Blue";
+
+const colors = ["red", "orange", "blue"];
+allLis.forEach((li, i) => {
+	const color = colors[i];
+	li.style.color = color;
+});
+
+// getComputedStyle
+const h1Style = getComputedStyle(h1);
+console.log(h1Style.color);
+console.log(h1Style.fontSize);
+
+
+// 
