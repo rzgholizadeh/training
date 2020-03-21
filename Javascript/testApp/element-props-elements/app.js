b82c5953b8af97859963947e09ae7da79fc70f4a
Reza <rzgholizadeh@gmail.com>
2020-03-20 20:48:40 -0600

    JS: manipulating style classes

--- Javascript/testApp/element-props-elements/app.js
+++ Javascript/testApp/element-props-elements/app.js
@@ -1,6 +1,7 @@
 // DOM manipulation
 const allLis = document.querySelectorAll("li");
 
+/*
 for (let i = 0; i < allLis.length; i++) {
 	allLis[i].innerText = "Some new text";
 }
@@ -9,23 +10,25 @@ for (let li of allLis) {
 	li.innerHTML = "another new <b> TEXT </b>";
 }
 
+*/
 // styles are camel cased
 const h1 = document.querySelector("h1");
 
 // add style value inline (html)
 // they override css style. it is better not to use them
 //h1.style.color = "Blue";
-
+/*
 const colors = ["red", "orange", "blue"];
 allLis.forEach((li, i) => {
 	const color = colors[i];
 	li.style.color = color;
 });
-
+*/
 // getComputedStyle
 const h1Style = getComputedStyle(h1);
 console.log(h1Style.color);
 console.log(h1Style.fontSize);
 
-
-// 
+// using classList
+const todo = document.querySelector("#todos .todo");
+todo.classList.toggle("todo");
