dbb3ade003584c7204a8726acd15e1d81bf4f6dc
Reza <rzgholizadeh@gmail.com>
2020-04-01 20:43:41 -0600

    JS: basic test example

--- Javascript/testing/hidash/index.js
+++ Javascript/testing/hidash/index.js
@@ -1,10 +1,13 @@
 module.exports = {
 	forEach(arr, fn) {
-		for (let i = 0; i < arr.length; i++) {
+		/*	for (let i = 0; i < arr.length; i++) {
 			const value = arr[i];
 			fn(value, i);
 		}
+        */
+
+		for (let index in arr) {
+			fn(arr[index], index);
+		}
 	}
 };
-
-
