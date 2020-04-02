4df8fb4585422ee07683341ce55b76a258b183ee
Reza <rzgholizadeh@gmail.com>
2020-04-01 20:53:02 -0600

    JS: test case for map function

--- Javascript/testing/hidash/index.test.js
+++ Javascript/testing/hidash/index.test.js
@@ -1,5 +1,6 @@
-const { forEach } = require("./index");
+const { forEach, map } = require("./index");
 
+// forEach
 let sum = 0;
 forEach([1, 2, 3], value => {
 	sum += value;
@@ -7,6 +8,21 @@ forEach([1, 2, 3], value => {
 
 if (sum !== 6) {
 	throw new Error("Expected summing array to equal 6");
-} else {
-	console.log("test passed");
+}
+
+// map
+const result = map([1, 2, 3], value => {
+	return value * 2;
+});
+
+if (result[0] !== 2) {
+	throw new Error(`Expected to find 2, but found ${result[0]}`);
+}
+
+if (result[1] !== 4) {
+	throw new Error(`Expected to find 2, but found ${result[1]}`);
+}
+
+if (result[2] !== 6) {
+	throw new Error(`Expected to find 2, but found ${result[2]}`);
 }
