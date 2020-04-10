dc66ab1d0c4856a4ae9aea8f59bc9ff6073cb9e7
Reza <rzgholizadeh@gmail.com>
2020-04-10 17:29:20 -0600

    node: Clustering node server

--- Javascript/node/cluster/index.js
+++ Javascript/node/cluster/index.js
@@ -1,14 +1,23 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-function doWork(duration) {
-	const start = Date.now();
-	while (Date.now() - start < duration) {}
+// is the file being executed in master mode?
+if (cluster.isMaster) {
+	// cause index.js to be executed again but in child mode
+	cluster.fork();
+} else {
+	// a child process to act like a server
+	const express = require("express");
+	const app = express();
+
+	function doWork(duration) {
+		const start = Date.now();
+		while (Date.now() - start < duration) {}
+	}
+
+	app.get("/", (req, res) => {
+		doWork(5000);
+		res.send("Hello beauty!");
+	});
+
+	app.listen(3000);
 }
-
-app.get("/", (req, res) => {
-	doWork(5000);
-	res.send("Hello beauty!");
-});
-
-app.listen(3000);
