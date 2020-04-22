e2e704a6ea67887f297876a5c85f1e38f4c1101f
Reza <rzgholizadeh@gmail.com>
2020-04-22 15:14:02 -0600

    React: Component Reusability

--- Javascript/react/components/src/index.js
+++ Javascript/react/components/src/index.js
@@ -1,29 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
 import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 3:00PM"
-                content="Good post man!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jennifer"
-                timeAgo="Today at 1:00PM"
-                content="Keep it up!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Carey"
-                timeAgo="Yesterday at 4:00AM"
-                content="Poor grammer"
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 3:00PM"
+                    content="Good post man!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jennifer"
+                    timeAgo="Today at 1:00PM"
+                    content="Keep it up!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Carey"
+                    timeAgo="Yesterday at 4:00AM"
+                    content="Poor grammer"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
             <div></div>
         </div>
     );
