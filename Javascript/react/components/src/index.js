13314ffae553f52c1df5225b63ace0460018d01b
Reza <rzgholizadeh@gmail.com>
2020-04-22 14:55:58 -0600

    React: Component Configurability

--- Javascript/react/components/src/index.js
+++ Javascript/react/components/src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 3:00PM"
+                content="Good post man!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Jennifer"
+                timeAgo="Today at 1:00PM"
+                content="Keep it up!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Carey"
+                timeAgo="Yesterday at 4:00AM"
+                content="Poor grammer"
+                avatar={faker.image.avatar()}
+            />
             <div></div>
         </div>
     );
