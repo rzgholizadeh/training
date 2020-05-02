be260c43a5e673f8d7c5bf197a62fa0b3a8d7d03
Reza <rzgholizadeh@gmail.com>
2020-05-02 17:35:36 -0600

    React: Add icon to the text

--- Javascript/react/classes/src/SeasonDisplay.js
+++ Javascript/react/classes/src/SeasonDisplay.js
@@ -10,7 +10,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    return <div>Season: {season}</div>;
+    const text = season === "winter" ? "It is Cold!" : "Summer Baby!";
+    const icon = season === "winter" ? "snowflake" : "sun";
+    return (
+        <div>
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
