d3837a6431482f2e8a98001228c4b1e991a1d63a
Reza <rzgholizadeh@gmail.com>
2020-05-04 18:09:12 -0600

    React: Controlled event handling

--- Javascript/react/user-events/src/components/SearchBar.js
+++ Javascript/react/user-events/src/components/SearchBar.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    state = { term: "" };
     render() {
         return (
             <div className="ui segment">
@@ -12,8 +10,9 @@ class SearchBar extends React.Component {
                         <label>Image Search</label>
                         <input
                             type="text"
-                            onChange={(event) =>
-                                console.log(event.target.value)
+                            value={this.state.term}
+                            onChange={(e) =>
+                                this.setState({ term: e.target.value })
                             }
                         />
                     </div>
