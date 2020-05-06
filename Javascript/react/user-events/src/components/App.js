a6c3a61d4866c925a88f8ced66b9f409dbdd9343
Reza <rzgholizadeh@gmail.com>
2020-05-05 19:09:35 -0600

    React: Get the fechted data and update state

--- Javascript/react/user-events/src/components/App.js
+++ Javascript/react/user-events/src/components/App.js
@@ -4,20 +4,26 @@ import keys from "../config/keys";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get("https://api.unsplash.com/search/photos", {
-            params: {
-                query: term
-            },
-            headers: {
-                Authorization: `Client-ID ${keys.unsplashAccessKey}`
+    state = { images: [] };
+    onSearchSubmit = async (term) => {
+        const response = await axios.get(
+            "https://api.unsplash.com/search/photos",
+            {
+                params: {
+                    query: term
+                },
+                headers: {
+                    Authorization: `Client-ID ${keys.unsplashAccessKey}`
+                }
             }
-        });
-    }
+        );
+        this.setState({ images: response.data.results });
+    };
     render() {
         return (
             <div className="ui container" style={{ marginTop: "10px" }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length}
             </div>
         );
     }
