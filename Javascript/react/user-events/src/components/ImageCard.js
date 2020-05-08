effcaffa165fea8946c551e4445545339c9016aa
Reza <rzgholizadeh@gmail.com>
2020-05-07 21:01:40 -0600

    React: Dynamic spans for images

--- Javascript/react/user-events/src/components/ImageCard.js
+++ Javascript/react/user-events/src/components/ImageCard.js
@@ -1,18 +1,23 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+    state = { spans: 0 };
     constructor(props) {
         super(props);
-
         this.imageRef = React.createRef();
     }
     componentDidMount() {
-        console.log(this.imageRef.current.clientHeight);
+        this.imageRef.current.addEventListener("load", this.setSpans);
     }
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10 + 1);
+        this.setState({ spans });
+    };
     render() {
         const { description, urls } = this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img ref={this.imageRef} alt={description} src={urls.regular} />
             </div>
         );
