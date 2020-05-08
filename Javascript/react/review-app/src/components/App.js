21836ad7712d55bf3b221e15a2aca7933973a179
Reza <rzgholizadeh@gmail.com>
2020-05-08 15:55:36 -0600

    React: Create components directory

---
+++ Javascript/react/review-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>Hello Kitty</div>
+            </div>
+        );
+    }
+}
+
+export default App;
