a53f6c31dff15228826687150f5535af475f658d
Reza <rzgholizadeh@gmail.com>
2020-05-08 15:59:29 -0600

    React: Add search bar component

--- Javascript/react/review-app/src/components/App.js
+++ Javascript/react/review-app/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import SearchBar from "./SearchBar";
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <div>Hello Kitty</div>
+                <SearchBar />
             </div>
         );
     }
---
+++ Javascript/react/review-app/src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>SearchBar</div>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
