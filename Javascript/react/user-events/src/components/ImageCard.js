00b613ca2093473f07d31ae12b1a6ba8402bc021
Reza <rzgholizadeh@gmail.com>
2020-05-07 20:47:11 -0600

    React: React Refs

--- Javascript/react/user-events/src/components/ImageCard.js
+++ Javascript/react/user-events/src/components/ImageCard.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+    componentDidMount() {
+        console.log(this.imageRef.current.clientHeight);
+    }
     render() {
+        const { description, urls } = this.props.image;
         return (
             <div>
-                <img
-                    alt={this.props.image.description}
-                    src={this.props.image.urls.regular}
-                />
+                <img ref={this.imageRef} alt={description} src={urls.regular} />
             </div>
         );
     }
