f872651ffe7f905040d016c1fe93c2847968aa87
Reza <rzgholizadeh@gmail.com>
2020-05-10 16:35:17 -0600

    React: Create Video Item component

--- Javascript/react/review-app/src/components/VideoList.js
+++ Javascript/react/review-app/src/components/VideoList.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const VideoList = (props) => {
-    const videos = props.videos;
-    return (
-        <div>
-            <div>{videos.length}</div>
-        </div>
-    );
+import VideoItem from "./VideoItem";
+
+const VideoList = ({ videos }) => {
+    const renderedList = videos.map((video) => {
+        return <VideoItem />;
+    });
+    return <div>{renderedList}</div>;
 };
 
 export default VideoList;
