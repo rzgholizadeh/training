a4dc6dd1717522b12c8833221e3c4093b0bba266
Reza <rzgholizadeh@gmail.com>
2020-05-10 17:01:13 -0600

    React: Selected video mechanism

--- Javascript/react/review-app/src/components/App.js
+++ Javascript/react/review-app/src/components/App.js
@@ -6,7 +6,7 @@ import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
     onTermSubmit = async (term) => {
         const response = await youtube.get("/search", {
             params: {
@@ -19,11 +19,19 @@ class App extends React.Component {
         });
         this.setState({ videos: response.data.items });
     };
+    onVideoSelect = (video) => {
+        console.log("From the app", video);
+
+        this.setState({ selectedVideo: video });
+    };
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList
+                    onVideoSelect={this.onVideoSelect}
+                    videos={this.state.videos}
+                />
             </div>
         );
     }
