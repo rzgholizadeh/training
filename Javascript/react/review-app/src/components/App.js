f077cd221f9a5e9961deb2da686578fb13a3af79
Reza <rzgholizadeh@gmail.com>
2020-05-10 17:08:27 -0600

    React: Rendering selected video

--- Javascript/react/review-app/src/components/App.js
+++ Javascript/react/review-app/src/components/App.js
@@ -4,6 +4,7 @@ import keys from "../keys";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -20,14 +21,13 @@ class App extends React.Component {
         this.setState({ videos: response.data.items });
     };
     onVideoSelect = (video) => {
-        console.log("From the app", video);
-
         this.setState({ selectedVideo: video });
     };
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={this.onVideoSelect}
                     videos={this.state.videos}
