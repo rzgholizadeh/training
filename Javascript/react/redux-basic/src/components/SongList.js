06aa385955b67f20df9e5b26dbb100a923f2f14f
Reza <rzgholizadeh@gmail.com>
2020-05-11 19:04:35 -0600

    React: Using connect function from react-redux

--- Javascript/react/redux-basic/src/components/SongList.js
+++ Javascript/react/redux-basic/src/components/SongList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class SongList extends React.Component {
     render() {
@@ -10,4 +11,9 @@ class SongList extends React.Component {
     }
 }
 
-export default SongList;
+const mapStateToProps = (state) => {
+    console.log(state);
+    return state;
+};
+
+export default connect(mapStateToProps)(SongList);
