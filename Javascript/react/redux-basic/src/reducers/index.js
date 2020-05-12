7335e8c49c43bcaebb5efd455b6fdec334b831ed
Reza <rzgholizadeh@gmail.com>
2020-05-11 18:50:45 -0600

    React: Combining reducers

--- Javascript/react/redux-basic/src/reducers/index.js
+++ Javascript/react/redux-basic/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 // A static reducer
 const songsReducer = () => {
     return [
@@ -15,3 +17,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
     }
     return selectedSong;
 };
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
