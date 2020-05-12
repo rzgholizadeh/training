f78c6eb0aef67e61a8917346cb92cb77834d47c3
Reza <rzgholizadeh@gmail.com>
2020-05-11 18:57:32 -0600

    React: Add songlist component

--- Javascript/react/redux-basic/src/components/App.js
+++ Javascript/react/redux-basic/src/components/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { selectSong } from "../actions";
+import SongList from "./SongList";
 
 const App = () => {
     return (
         <div>
-            <div></div>
+            <SongList />
         </div>
     );
 };
---
+++ Javascript/react/redux-basic/src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SongList extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>SongList</div>
+            </div>
+        );
+    }
+}
+
+export default SongList;
